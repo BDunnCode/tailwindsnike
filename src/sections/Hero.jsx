6bbaeb6f291ce6c2d0c9c80ce3efb4c5bec9e852
BDunnCode <BDunnCode@github.com>
2024-02-24 21:34:12 -0600

    hero done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,13 @@
 import Button from "../components/Button"
 import { arrowRight } from "../../assets/icons"
-import { statistics } from "../constants"
+import { statistics, shoes } from "../constants"
+import { bigShoe1 }  from "../../assets/images"
+import ShoeCard from "../components/ShoeCard"
+import { useState } from "react"
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
+
   return (
       <section id="home" 
         className="w-full flex flex-col xl:flex-row 
@@ -26,19 +31,42 @@ const Hero = () => {
           <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
             {statistics.map((stat) => (
               <div className="flex-col" key={stat.label}>
-                <p className="text-4xl 
-                font-palanquin 
-                font-bold">{stat.value}
+                <p className="text-4xl font-palanquin font-bold">
+                  {stat.value}
                 </p>
-                <p className="leading-7 
-                font-montserrat
-                text-slate-gray">{stat.label}
+                <p className="leading-7 font-montserrat text-slate-gray">
+                  {stat.label}
                 </p>
               </div>
             ))}
           </div>
         </div>
   
+        <div className="relative flex-1
+        flex justify-center
+        items-center xl:min-h-screen max-xl:py-40 bg-primary
+        bg-hero bg-cover bg-center">
+          <img  
+            src={bigShoeImg}
+            alt="shoe collection"
+            width={610}
+            height={500}
+            className="object-contain
+            relative z-10"
+           />
+
+          <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%]">
+            {shoes.map((shoe) => (
+              <div key={shoe}>
+                <ShoeCard
+                  imgURL={shoe}
+                  changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                  bigShoeImg={bigShoeImg}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
 
       </section>
     )

a6f405e020f4b14a05dca72b9e22309691a2960e
BDunnCode <BDunnCode@github.com>
2024-02-26 14:33:57 -0600

    SuperQuality section complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,16 @@ const Button = ({ label, iconURL }) => {
     <button className="flex items-center rounded-full bg-coral-red p-4
     px-7 py-4 border font-montserrat text-lg leading-none text-white">
       {label}
-      <img 
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
-    </button>
-    )
-}
 
-export default Button
+      {iconURL && (
+        <img 
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
